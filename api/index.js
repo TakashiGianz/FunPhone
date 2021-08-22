e0f035981b9d7b285b85acc443e0298c40895bd6
Takashi Gianz <fernando.takashi12@gmail.com>
2021-08-22 17:09:30 -0300

    First Version

--- api/index.js
+++ api/index.js
@@ -1,1 +1,29 @@
-console.log("Oila")
+import express from "express";
+import cors from "cors"
+const app = express();
+const port = 3000;
+
+app.use(cors())
+
+app.use("/", express.static("console"));
+app.use("/mobile", express.static("gamepad"));
+
+app.get("/api", (req, res) => {
+  res.json({ sala: "12345678" });
+});
+
+app.post("/api", (req, res) => {
+  res.json({ nome: "Post" });
+});
+
+app.put("/api", (req, res) => {
+  res.json({ nome: "Put" });
+});
+
+app.delete("/api", (req, res) => {
+  res.json({ nome: "Delete" });
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
