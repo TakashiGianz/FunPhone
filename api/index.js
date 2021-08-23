a09c598f800474c908707a52d58e15d17bd0f4f8
Fernando-Escritorio <fernando.takashi12@gmail.com>
2021-08-23 20:03:47 -0300

    refreshingUsers

--- api/index.js
+++ api/index.js
@@ -1,20 +1,20 @@
 import express from "express";
-import cors from "cors"
+import cors from "cors";
 const app = express();
 const port = 3000;
 
-app.use(cors())
+app.use(cors());
 
 const options = {
-  index: "connected.html"
-}
+  index: "connected.html",
+};
 
 app.use("/", express.static("console"));
 app.use("/mobile", express.static("gamepad"));
 app.use("/mobile/connected", express.static("gamepad", options));
 
 app.get("/api", (req, res) => {
-  res.json({ sala: "22334455" });
+  res.json({ sala: "22334455", usersOn: ["Fernando", "Camila", "Fabio", "Mariane", "Ellen"] });
 });
 
 app.post("/api", (req, res) => {
